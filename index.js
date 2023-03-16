05f5aa142b5644503d12dc117ecb321b3011dc8c
rachmanridwan <90601251+rachmanridwan@users.noreply.github.com>
2023-03-16 22:58:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
+
+const contacts = [{
+
+}]    
     
     const server = Hapi.server ({
         port: 3000,
@@ -10,6 +14,53 @@ const init = async () => {
     await server.start();
     console.log('Server running on $s', server.info.uri);
 }
+
+    server.route({
+        method: 'GET',
+        path: '/contact',
+        handler: (request, h) => {
+            return contacts;
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/contact/{id}',
+        handler: (request, h) => {
+            const contact = contacts.find(b => b.id === parseInt(request.params.id));
+            if (!contact) {
+                return h.response('Book not found').code(404);
+            }
+            return contact;
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/contact',
+        handler: (request, h) => {
+            const book = {
+                id: contacts.length + 1,
+                title: request.payload.title,
+                author: request.payload.author
+            };
+            contacts.push(contact);
+            return contact;
+        }
+    });
+
+    server.route({
+        method: 'DELETE',
+        path: '/contact/{id}',
+        handler: (request, h) => {
+            const contactIndex = contacts.findIndex(b => b.id === parseInt(request.params.id));
+            if (contactIndex === -1) {
+                return h.response('Book not found').code(404);
+            }
+            contacts.splice(bookIndex, 1);
+            return h.response().code(204);
+        }
+    });
 
 process.on('unhandledRejection', (err) => {
     console.log(err);
